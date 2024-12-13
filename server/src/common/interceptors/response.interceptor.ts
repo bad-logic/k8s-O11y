edb3f14f9f70d32b361bbf3ac1a4853f2620935b
roshan <45297269+bad-logic@users.noreply.github.com>
2024-12-12 21:37:51 -0600

    chore: interceptor and validation

---
+++ server/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+// Aspect oriented programming
+
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    console.log('Before....');
+    return next.handle().pipe(tap((data) => console.log('After ...', data)));
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WrapResponseInterceptor } from './common/interceptors/response.interceptor';
+// import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     transform: true,
+  //     forbidNonWhitelisted: true,
+  //     transformOptions: {
+  //       enableImplicitConversion: true,
+  //     },
+  //   }),
+  // );
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
