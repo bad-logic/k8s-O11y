4eec0f55aca93140f4a1b39be439c3f7e92c9e5d
roshan <45297269+bad-logic@users.noreply.github.com>
2024-12-15 17:16:31 -0600

    chore: add @Set() decorator

---
+++ server/src/common/decorators/set.decorator.ts
@@ -0,0 +1,65 @@
+/**
+ *
+ * Maps the key to the path.
+ * object[key] = object[path].
+ *
+ * @param path property name
+ *
+ * @returns
+ *
+ * Example:
+```
+class Test{
+
+    private secret: string;
+
+    `@Set('secret')`
+    public exposed: string;
+
+    // will add the following function to the class
+    // public set exposed(val){
+    //     return this.secret=val;
+    // }
+
+}
+const o = new Test();
+o.exposed = 'newSecretValue';
+```
+ *
+ * 
+ *
+ */
+export function Set(path: string) {
+  return (target: any, key: string) => {
+    if (path === key) {
+      // will call itself and result in recursive calls
+      throw new Error(`Duplicate Identifier \'${path}\'`);
+    }
+
+    Object.defineProperty(target, key, {
+      set: function (val) {
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        let ref = this;
+        const splitter = path.split('.');
+        let count = 1;
+        for (const ind of splitter) {
+          if (count === splitter.length) {
+            ref[ind] = val;
+            break;
+          }
+
+          if (ref[ind] === undefined) {
+            throw new Error(
+              `path ${path} doesn't exists in ${this.constructor.name}`,
+            );
+          }
+
+          ref = ref[ind];
+          count++;
+        }
+      },
+      configurable: true,
+      enumerable: false,
+    });
+  };
+}
---
+++ server/src/common/decorators/set.spec.ts
@@ -0,0 +1,75 @@
+import { Get } from './get.decorator';
+import { Set } from './set.decorator';
+
+describe('Testing @Set() Decorator', () => {
+  describe('Testing @Set() decorator independently', () => {
+    class Test {
+      public readonly secret;
+
+      @Set('secret')
+      public exposed: number;
+
+      constructor(val: number) {
+        this.secret = val;
+      }
+    }
+
+    it('it should set the original key', () => {
+      const o = new Test(9);
+      expect(o.secret).toEqual(9);
+    });
+
+    it('it should change the value of the original key on update', () => {
+      const o = new Test(9);
+      o.exposed = 99;
+      expect(o.secret).toEqual(99);
+    });
+
+    it('it should return undefined on accessing the alias key', () => {
+      const o = new Test(9);
+      expect(o.exposed).not.toBeDefined();
+    });
+
+    it('it should return undefined on accessing the alias key after the update', () => {
+      const o = new Test(9);
+      o.exposed = 100;
+      expect(o.exposed).not.toBeDefined();
+    });
+  });
+
+  describe('Testing @Set() while also using @Get() decorator to the same value', () => {
+    class Test {
+      public readonly secret;
+
+      @Get('secret')
+      @Set('secret')
+      public exposed: number;
+
+      constructor(val: number) {
+        this.secret = val;
+      }
+    }
+
+    it('it should set the original key', () => {
+      const o = new Test(9);
+      expect(o.secret).toEqual(9);
+    });
+
+    it('it should change the value of the original key on update', () => {
+      const o = new Test(9);
+      o.exposed = 99;
+      expect(o.secret).toEqual(99);
+    });
+
+    it('it should return the original key value on accessing the alias key', () => {
+      const o = new Test(9);
+      expect(o.exposed).toEqual(9);
+    });
+
+    it('it should return the original key value on accessing the alias key after the update', () => {
+      const o = new Test(9);
+      o.exposed = 100;
+      expect(o.exposed).toEqual(100);
+    });
+  });
+});
