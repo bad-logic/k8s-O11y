4c751777091d15ed91b94a69e3a252a80c0ad5ea
roshan <45297269+bad-logic@users.noreply.github.com>
2024-12-14 19:47:39 -0600

    chore: add helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WrapResponseInterceptor } from './common/interceptors/response.interceptor';
 import { ConfigurationService } from './common/config/config.service';
+import helmet from 'helmet';
 // import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
 
   const configService = app.get(ConfigurationService);
+
   // app.useGlobalPipes(
   //   new ValidationPipe({
   //     whitelist: true,
