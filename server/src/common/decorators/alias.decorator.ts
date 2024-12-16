6730bc90df700753d931f60ded57b9f63957c26d
roshan <45297269+bad-logic@users.noreply.github.com>
2024-12-15 22:21:20 -0600

    chore: add @Alias() test cases

--- server/src/common/decorators/alias.decorator.ts
+++ server/src/common/decorators/alias.decorator.ts
@@ -39,40 +39,39 @@ export function Alias(path: string) {
   return (target: any, key: string) => {
     if (path === key) {
       // will call itself and result in recursive calls
-      throw new Error(`Duplicate Identifier \'${path}\'`);
+      const err = new Error(`Duplicate Identifier \'${path}\'`);
+      err.name = 'DuplicateError';
+      throw err;
     }
 
     Object.defineProperty(target, key, {
       get: function () {
         return path.split('.').reduce((p, n) => {
-          if (p === undefined || p[n] === undefined) {
-            throw new Error(
+          if (typeof p !== 'object') {
+            const err = new Error(
               `path ${path} doesn't exists in ${this.constructor.name}`,
             );
+            err.name = 'PathNotFoundError';
+            throw err;
           }
           return p[n];
         }, this);
       },
       set: function (val) {
-        // eslint-disable-next-line @typescript-eslint/no-this-alias
-        let ref = this;
         const splitter = path.split('.');
-        let count = 1;
-        for (const ind of splitter) {
-          if (count === splitter.length) {
-            ref[ind] = val;
-            break;
-          }
-
-          if (ref[ind] === undefined) {
-            throw new Error(
+        splitter.reduce((p, n, i) => {
+          if (typeof p !== 'object') {
+            const err = new Error(
               `path ${path} doesn't exists in ${this.constructor.name}`,
             );
+            err.name = 'PathNotFoundError';
+            throw err;
           }
-
-          ref = ref[ind];
-          count++;
-        }
+          if (i == splitter.length - 1) {
+            p[n] = val;
+          }
+          return p[n];
+        }, this);
       },
       configurable: true,
       enumerable: false,
--- server/src/common/decorators/alias.spec.ts
+++ server/src/common/decorators/alias.spec.ts
@@ -1,25 +1,147 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Alias } from './alias.decorator';
 
 describe('Testing @Alias() Decorator', () => {
-  class Test {
-    private readonly secret;
+  describe('Testing with simple value', () => {
+    class Test {
+      public readonly secret;
 
-    @Alias('secret')
-    public exposed: number;
+      @Alias('secret')
+      public exposed: number;
 
-    constructor(val: number) {
-      this.secret = val;
+      @Alias('secret.notfound')
+      public notfound: number;
+
+      constructor(val: number) {
+        this.secret = val;
+      }
     }
-  }
 
-  it('should return original value on accessing the alias key', () => {
-    const o = new Test(9);
-    expect(o.exposed).toEqual(9);
+    it('should throw DuplicateError if the path and key are same', () => {
+      try {
+        class Test {
+          @Alias('value')
+          public value: number;
+        }
+        new Test();
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+        expect(err.name).toEqual('DuplicateError');
+      }
+    });
+
+    it('should throw PathNotFoundError while accessing invalid path', () => {
+      const o = new Test(9);
+      try {
+        const _ = o.notfound;
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+        expect(err.name).toBe('PathNotFoundError');
+      }
+    });
+
+    it('should throw PathNotFoundError on updating invalid path', () => {
+      const o = new Test(9);
+      try {
+        o.notfound = 56;
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+        expect(err.name).toBe('PathNotFoundError');
+      }
+    });
+
+    it('should return the value of path while accessing the alias key', () => {
+      const o = new Test(9);
+      expect(o.exposed).toEqual(o.secret);
+    });
+
+    it('should successfully update the path value through alias key', () => {
+      const o = new Test(9);
+      o.exposed = 89;
+      expect(o.secret).toEqual(89);
+    });
+
+    it('should successfully get the updated path value from the alias key', () => {
+      const o = new Test(9);
+      o.exposed = 89;
+      expect(o.exposed).toEqual(o.secret);
+    });
   });
 
-  it('should return updated value on accessing the alias key after the update', () => {
-    const o = new Test(9);
-    o.exposed = 100;
-    expect(o.exposed).toEqual(100);
+  describe('Testing with nested objects', () => {
+    class Test {
+      public readonly data = {};
+
+      @Alias('data.first.second.third.fourth.fifth.sixth.seventh.secret')
+      public exposed: number;
+
+      @Alias('data.notfound')
+      public notfound: number;
+
+      constructor(val: number) {
+        this.data = {
+          first: {
+            second: {
+              third: {
+                fourth: {
+                  fifth: {
+                    sixth: {
+                      seventh: {
+                        secret: val,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            message: 'doomed',
+            status: 'nested',
+          },
+        };
+      }
+    }
+
+    it('should throw PathNotFoundError while accessing invalid path', () => {
+      const o = new Test(9);
+      try {
+        const _ = o.notfound;
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+        expect(err.name).toBe('PathNotFoundError');
+      }
+    });
+
+    it('should throw PathNotFoundError on updating invalid path', () => {
+      const o = new Test(9);
+      try {
+        o.notfound = 56;
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+        expect(err.name).toBe('PathNotFoundError');
+      }
+    });
+
+    it('should return the value of path while accessing the alias key', () => {
+      const o = new Test(9);
+      expect(o.exposed).toEqual(
+        (o.data as any).first.second.third.fourth.fifth.sixth.seventh.secret,
+      );
+    });
+
+    it('should successfully update the path value through alias key', () => {
+      const o = new Test(9);
+      o.exposed = 89;
+      expect(
+        (o.data as any).first.second.third.fourth.fifth.sixth.seventh.secret,
+      ).toEqual(89);
+    });
+
+    it('should successfully get the updated path value from the alias key', () => {
+      const o = new Test(9);
+      o.exposed = 89;
+      expect(o.exposed).toEqual(
+        (o.data as any).first.second.third.fourth.fifth.sixth.seventh.secret,
+      );
+    });
   });
 });
