03bf0e3bf57d9731d38470de41c4af3cfea0cedc
roshan <45297269+bad-logic@users.noreply.github.com>
2024-12-17 11:09:33 -0600

    chore: update and apply middlewares in app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,24 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { DomainModule } from './domain/domain.module';
 import { ConfigModule } from './common/modules/config/config.module';
 import { AppLoggerModule } from './common/modules/logger/Applogger.module';
-import { CorrelationIdMiddleware } from './common/modules/logger/correlationId.middleware';
+import { CorrelationIdMiddleware } from './common/modules/middlewares/correlationId.middleware';
+import { MiddlewareModule } from './common/modules/middlewares/middlewares.module';
+import { RequestLoggerMiddleware } from './common/modules/middlewares/requestLogger.middleware';
+import { InjectableModule } from './common/modules/injectables/injectable.module';
 
 @Module({
-  imports: [DomainModule, ConfigModule, AppLoggerModule],
+  imports: [
+    DomainModule,
+    ConfigModule,
+    InjectableModule,
+    AppLoggerModule,
+    MiddlewareModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(CorrelationIdMiddleware).forRoutes('*');
+    consumer
+      .apply(CorrelationIdMiddleware, RequestLoggerMiddleware)
+      .forRoutes('*');
   }
 }
